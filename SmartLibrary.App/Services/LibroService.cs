13484dc7120760cd34c11fbbde3d7e086826f35c
MAICOL ESNEIDER <kingofthieves465@gmail.com>
2026-04-06 12:50:39 -0500

    fix: correct class structure and generic syntax in LibroService

--- SmartLibrary.App/Services/LibroService.cs
+++ SmartLibrary.App/Services/LibroService.cs
@@ -7,21 +7,20 @@ namespace SmartLibrary.App.Services
     public class LibroService
     {
         private List<Libro> libros = new List<Libro>();
+
         public void AgregarLibro(Libro libro)
-{
-    libros.Add(libro);
-}
+        {
+            libros.Add(libro);
+        }
 
-public void EliminarLibro(Libro libro)
-{
-    libros.Remove(libro);
-}
+        public void EliminarLibro(Libro libro)
+        {
+            libros.Remove(libro);
+        }
 
-public List<Libro> ObtenerTodos()
-{
-    return libros;
-}
-
-
+        public List<Libro> ObtenerTodos()
+        {
+            return libros;
+        }
     }
 }
