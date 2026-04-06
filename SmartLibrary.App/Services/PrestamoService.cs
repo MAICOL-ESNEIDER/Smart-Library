61e9fd88d9c1cc6a0efd4a031ecb4744f878c3d1
MAICOL ESNEIDER <kingofthieves465@gmail.com>
2026-04-06 13:35:40 -0500

    feat: implement add, remove and get all methods in PrestamoService

--- SmartLibrary.App/Services/PrestamoService.cs
+++ SmartLibrary.App/Services/PrestamoService.cs
@@ -7,5 +7,20 @@ namespace SmartLibrary.App.Services
     public class PrestamoService
     {
         private readonly List<Prestamo> prestamos = new List<Prestamo>();
+
+        public void AgregarPrestamo(Prestamo prestamo)
+        {
+            prestamos.Add(prestamo);
+        }
+
+        public void EliminarPrestamo(Prestamo prestamo)
+        {
+            prestamos.Remove(prestamo);
+        }
+
+        public List<Prestamo> ObtenerTodos()
+        {
+            return prestamos;
+        }
     }
 }
