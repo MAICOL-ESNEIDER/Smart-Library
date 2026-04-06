7b35163aba6e7d8b22c5c58110545ffaf2e080cf
MAICOL ESNEIDER <kingofthieves465@gmail.com>
2026-04-06 13:18:37 -0500

    feat: implement add, remove and get all methods in UsuarioService

--- SmartLibrary.App/Services/UsuarioService.cs
+++ SmartLibrary.App/Services/UsuarioService.cs
@@ -7,5 +7,20 @@ namespace SmartLibrary.App.Services
     public class UsuarioService
     {
         private readonly List<Usuario> usuarios = new List<Usuario>();
+
+        public void AgregarUsuario(Usuario usuario)
+        {
+            usuarios.Add(usuario);
+        }
+
+        public void EliminarUsuario(Usuario usuario)
+        {
+            usuarios.Remove(usuario);
+        }
+
+        public List<Usuario> ObtenerTodos()
+        {
+            return usuarios;
+        }
     }
 }
